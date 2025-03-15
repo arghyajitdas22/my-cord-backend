1519a3c3734b47474f3004b53d8e934a3ddda758
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2025-03-15 21:07:00 +0530

    added logout functionality

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -56,11 +56,11 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
   const validatedUser = loginUserSchema.parse(receivedUser);
   const user = await User.findOne({ email: validatedUser.email });
   if (!user) {
-    throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
+    throw new ApiError(StatusCodes.BAD_REQUEST, "User not found");
   }
   const isPasswordCorect = await user.comparePassword(validatedUser.password);
   if (!isPasswordCorect) {
-    throw new ApiError(StatusCodes.UNAUTHORIZED, "Incorrect password");
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Incorrect password");
   }
   const accessToken = user.generateAccessToken();
   const refreshToken = user.generateRefreshToken();
@@ -81,46 +81,62 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
     );
 });
 
-const refreshTokens = asyncHandler(
-  async (req: AuthenticatedRequest, res: Response) => {
-    const incomingrefreshToken = req.cookies.refreshToken;
-    if (!incomingrefreshToken) {
-      throw new ApiError(StatusCodes.UNAUTHORIZED, "Refresh token is missing");
-    }
-
-    try {
-      const deodedRefreshToken = jwt.verify(
-        incomingrefreshToken,
-        process.env.REFRESH_TOKEN_SECRET as string
-      );
-      const user = await User.findById((deodedRefreshToken as any).userId);
-      if (!user) {
-        throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
-      }
-      if (user.refreshToken !== incomingrefreshToken) {
-        throw new ApiError(StatusCodes.UNAUTHORIZED, "Refresh Token Expired");
-      }
-      const accessToken = user.generateAccessToken();
-      const refreshToken = user.generateRefreshToken();
-      user.refreshToken = refreshToken;
-      await user.save({ validateBeforeSave: false });
-      const loggedInUser = await User.findById(user._id).select(
-        "-password -refreshToken"
-      );
-      return res
-        .status(StatusCodes.OK)
-        .cookie("accessToken", accessToken, accessTokenOptions)
-        .cookie("refreshToken", refreshToken, refreshTokenOptions)
-        .json(
-          new ApiResponse(StatusCodes.OK, "Tokens have been refreshed", {
-            user: loggedInUser,
-            accessToken,
-          })
-        );
-    } catch (error) {
-      throw new ApiError(StatusCodes.UNAUTHORIZED, "Invalid refresh token");
-    }
+const refreshTokens = asyncHandler(async (req: Request, res: Response) => {
+  const incomingrefreshToken = req.cookies.refreshToken;
+  if (!incomingrefreshToken) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Refresh token is missing");
   }
-);
 
-export { registerUser, loginUser, refreshTokens };
+  try {
+    const deodedRefreshToken = jwt.verify(
+      incomingrefreshToken,
+      process.env.REFRESH_TOKEN_SECRET as string
+    );
+    const user = await User.findById((deodedRefreshToken as any).userId);
+    if (!user) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "User not found");
+    }
+    if (user.refreshToken !== incomingrefreshToken) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Refresh Token Expired");
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+    return res
+      .status(StatusCodes.OK)
+      .cookie("accessToken", accessToken, accessTokenOptions)
+      .cookie("refreshToken", refreshToken, refreshTokenOptions)
+      .json(
+        new ApiResponse(StatusCodes.OK, "Tokens have been refreshed", {
+          user: loggedInUser,
+          accessToken,
+        })
+      );
+  } catch (error) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Invalid refresh token");
+  }
+});
+
+const logoutUSer = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.body.userId;
+  if (!userId) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "User Id missing");
+  }
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Invalid user id");
+  }
+  user.refreshToken = "";
+  await user.save({ validateBeforeSave: false });
+  return res
+    .clearCookie("accessToken", accessTokenOptions)
+    .clearCookie("refreshToken", refreshTokenOptions)
+    .status(StatusCodes.OK)
+    .json(new ApiResponse(StatusCodes.OK, "User successfully logged out", {}));
+});
+
+export { registerUser, loginUser, refreshTokens, logoutUSer };
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   refreshTokens,
+  logoutUSer,
 } from "../controllers/auth.controller";
 import { verifyAccessToken } from "../middlewares/auth.middleware";
 
@@ -10,6 +11,7 @@ const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/refresh-token").post(verifyAccessToken, refreshTokens);
+router.route("/refresh-token").post(refreshTokens);
+router.route("/logout").post(logoutUSer);
 
 export default router;
