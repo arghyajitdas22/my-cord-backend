26ad2bb40ce0ce26e482fe8e43e16678d22a3a66
Arghyajit Das <116450956+arghyajitdas22@users.noreply.github.com>
2025-04-14 22:33:30 +0530

    completed and ested add members, change member role, remove meber api

--- src/controllers/server.controller.ts
+++ src/controllers/server.controller.ts
@@ -34,12 +34,207 @@ export const createServer = asyncHandler(
 
     server.save({ validateBeforeSave: false });
 
+    res.status(StatusCodes.CREATED).json(
+      new ApiResponse(StatusCodes.CREATED, "Server has been created", {
+        server,
+      })
+    );
+  }
+);
+
+export const addMembersToServer = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { serverId } = req.params;
+    const { memberIds } = req.body;
+
+    if (!memberIds || memberIds.length === 0) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Please provide member ids");
+    }
+
+    const server = await Server.findById(serverId);
+    if (!server) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Server not found");
+    }
+
+    const userId = req.user?._id;
+    if (!userId) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, "User not found");
+    }
+
+    const filteredMembers = server.members.filter(
+      (member) => member.user.toString() === userId.toString()
+    );
+    if (filteredMembers.length === 0) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "You are not a member of this server"
+      );
+    }
+    if (
+      filteredMembers[0].role !== "admin" &&
+      filteredMembers[0].role !== "owner"
+    ) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "You are not authorized to add members to this server"
+      );
+    }
+
+    const usersToAdd = await User.find({ _id: { $in: memberIds } });
+    if (usersToAdd.length !== memberIds.length) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Some members not found");
+    }
+
+    const membersAlreadyInServer = server.members.filter((member) =>
+      memberIds.includes(member.user.toString())
+    );
+    if (membersAlreadyInServer.length > 0) {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        "Some members are already in the server"
+      );
+    }
+
+    usersToAdd.forEach((user) => {
+      server.members.push({ user: user._id, role: "member" });
+    });
+    await server.save({ validateBeforeSave: false });
+
     res
-      .status(StatusCodes.CREATED)
+      .status(StatusCodes.OK)
+      .json(new ApiResponse(StatusCodes.OK, "Members added to server", server));
+  }
+);
+
+export const changeMemberRole = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { serverId } = req.params;
+    const { role, memberId } = req.body;
+
+    if (!role || !["admin", "member"].includes(role)) {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        "Please provide a valid role"
+      );
+    }
+
+    const server = await Server.findById(serverId);
+    if (!server) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Server not found");
+    }
+
+    const userId = req.user?._id;
+    if (!userId) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, "User not found");
+    }
+
+    const filteredMembers = server.members.filter(
+      (member) => member.user.toString() === userId.toString()
+    );
+    if (filteredMembers.length === 0) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "You are not a member of this server"
+      );
+    }
+    if (
+      filteredMembers[0].role !== "admin" &&
+      filteredMembers[0].role !== "owner"
+    ) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "You are not authorized to change member role in this server"
+      );
+    }
+
+    const memberToChangeRole = server.members.find(
+      (member) => member.user.toString() === memberId.toString()
+    );
+    if (!memberToChangeRole) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Member not found in server");
+    }
+    if (memberToChangeRole.role === "owner") {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        "You cannot change the role of the owner"
+      );
+    }
+    if (memberToChangeRole.role === role) {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        "Member already has this role"
+      );
+    }
+
+    memberToChangeRole.role = role;
+    await server.save({ validateBeforeSave: false });
+
+    res
+      .status(StatusCodes.OK)
+      .json(new ApiResponse(StatusCodes.OK, "Member role changed", server));
+  }
+);
+
+export const removeMemeberFromServer = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response) => {
+    const { serverId } = req.params;
+    const { memberId } = req.body;
+
+    if (!memberId) {
+      throw new ApiError(StatusCodes.BAD_REQUEST, "Please provide member id");
+    }
+
+    const server = await Server.findById(serverId);
+    if (!server) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Server not found");
+    }
+
+    const userId = req.user?._id;
+    if (!userId) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, "User not found");
+    }
+
+    const filteredMembers = server.members.filter(
+      (member) => member.user.toString() === userId.toString()
+    );
+    if (filteredMembers.length === 0) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "You are not a member of this server"
+      );
+    }
+    if (
+      filteredMembers[0].role !== "admin" &&
+      filteredMembers[0].role !== "owner"
+    ) {
+      throw new ApiError(
+        StatusCodes.UNAUTHORIZED,
+        "You are not authorized to remove members from this server"
+      );
+    }
+
+    const memberToRemove = server.members.find(
+      (member) => member.user.toString() === memberId.toString()
+    );
+    if (!memberToRemove) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Member not found in server");
+    }
+    if (memberToRemove.role === "owner") {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        "You cannot remove the owner from the server"
+      );
+    }
+
+    server.members = server.members.filter(
+      (member) => member.user.toString() !== memberId.toString()
+    );
+    await server.save({ validateBeforeSave: false });
+
+    res
+      .status(StatusCodes.OK)
       .json(
-        new ApiResponse(StatusCodes.CREATED, "Server has been created", {
-          server,
-        })
+        new ApiResponse(StatusCodes.OK, "Member removed from server", server)
       );
   }
 );
--- src/routes/server.route.ts
+++ src/routes/server.route.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { createServer } from "../controllers/server.controller";
+import {
+  addMembersToServer,
+  changeMemberRole,
+  createServer,
+  removeMemeberFromServer,
+} from "../controllers/server.controller";
 import { verifyAccessToken } from "../middlewares/auth.middleware";
 import router from "./healthcheck.route";
 import { upload } from "../middlewares/multer.middleware";
@@ -7,5 +12,17 @@ import { upload } from "../middlewares/multer.middleware";
 router
   .route("/")
   .post(verifyAccessToken, upload.single("avatar"), createServer);
+
+router
+  .route("/add-members/:serverId")
+  .patch(verifyAccessToken, addMembersToServer);
+
+router
+  .route("/change-role/:serverId")
+  .patch(verifyAccessToken, changeMemberRole);
+
+router
+  .route("/remove-member/:serverId")
+  .patch(verifyAccessToken, removeMemeberFromServer);
 
 export default router;
